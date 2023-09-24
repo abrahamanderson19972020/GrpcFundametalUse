1d6581a540e137a6b94acbcbef567a552af2b85d
Abraham Anderson <abraham.anderson83@gmail.com>
2023-09-24 14:18:36 +0200

    Grps Server and Client Side are added.

---
+++ GrpcDemo/GrpcClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+
+Console.WriteLine("Hello World");
+
+//HelloRequest request = new HelloRequest()
+//{
+//    Name = "Prophet Mohammad"
+//};
+//var channel = GrpcChannel.ForAddress("https://localhost:7030");
+////Grpc Service Call to create client that will use this communication channel
+//var client = new Greeter.GreeterClient(channel);
+//var reply = await client.SayHelloAsync(request);
+//Console.WriteLine(reply);
+//Console.WriteLine(reply.Message);
+CustomerRequest request = new CustomerRequest()
+{
+    UserId = 3
+};
+var channel = GrpcChannel.ForAddress("https://localhost:7030");
+var client = new Customer.CustomerClient(channel);
+var response = client.GetCustomerInfo(request);
+Console.WriteLine(response.ToString());
+Console.WriteLine($"Customer Id:{request.UserId} Customer Name: {response.FirstName} {response.LastName} with age {response.Age} and address: {response.Address}. Is alive: {response.IsAlive}");
+Console.WriteLine("..................Customer List.................");
+List<CustomerResponse> responseList = new List<CustomerResponse>();
+using (var call = client.GetAllCustomers(new AllCustomersRequest()))
+{
+    while(await call.ResponseStream.MoveNext())
+    {
+        var currentCustomer = call.ResponseStream.Current;
+        Console.WriteLine($"Customer Id:{currentCustomer.CustomerId} Customer Name: {currentCustomer.FirstName} {currentCustomer.LastName} with age {currentCustomer.Age} and address: {currentCustomer.Address}. Is alive: {currentCustomer.IsAlive}");
+        responseList.Add(currentCustomer);
+    }
+}
+Console.ReadLine();
---
+++ GrpcDemo/GrpcServer/Services/CustomerService.cs
@@ -0,0 +1,90 @@
+﻿using Grpc.Core;
+
+namespace GrpcServer.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        public readonly ILogger<CustomerService> _logger;
+        List<CustomerResponse> customers = new List<CustomerResponse>()
+        {
+            new CustomerResponse()
+            {
+                 FirstName = "Abraham",
+                 LastName = "Anderson",
+                 Address = "Kristiansand/Norway",
+                 Age = 39,
+                 Email = "abraham@gmail.com",
+                 IsAlive = true,
+                 CustomerId = 1
+            },
+            new CustomerResponse()
+            {
+                 FirstName = "Karen",
+                 LastName = "Anderson",
+                 Address = "Kristiansand/Norway",
+                 Age = 35,
+                 Email = "karen@gmail.com",
+                 IsAlive = true,
+                 CustomerId = 2
+            },
+            new CustomerResponse()
+            {
+                 FirstName = "Naim",
+                 LastName = "Anderson",
+                 Address = "Kristiansand/Norway",
+                 Age = 11,
+                 Email = "naim@gmail.com",
+                 IsAlive = true,
+                 CustomerId = 3
+            },
+            new CustomerResponse()
+            {
+                 FirstName = "Pinar",
+                 LastName = "Anderson",
+                 Address = "Kristiansand/Norway",
+                 Age = 7,
+                 Email = "pinar@gmail.com",
+                 IsAlive = true,
+                 CustomerId = 4
+            },
+        };
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerResponse> GetCustomerInfo(CustomerRequest request, ServerCallContext context)
+        {
+            CustomerResponse customerResponse = new CustomerResponse();
+
+            if(request.UserId == 1)
+            {
+                customerResponse = customers.FirstOrDefault(c => c.CustomerId == 1);
+            }
+            else if(request.UserId == 2)
+            {
+                customerResponse = customers.FirstOrDefault(c => c.CustomerId == 2);
+            }
+            else  if(request.UserId==3)
+            {
+                customerResponse = customers.FirstOrDefault(c => c.CustomerId == 3);
+            }
+            else
+            {
+                customerResponse = customers.FirstOrDefault(c => c.CustomerId == 4);
+            }
+            _logger.LogInformation("Customer Response is ok:", customerResponse.ToString());
+            return Task.FromResult(customerResponse);
+        }
+
+        public override async Task GetAllCustomers(AllCustomersRequest request, IServerStreamWriter<CustomerResponse> responseStream, ServerCallContext context)
+        {
+           foreach(CustomerResponse customer in customers)
+            {
+                await Task.Delay(2000);
+                    await responseStream.WriteAsync(customer);
+            }
+        }
+
+    }
+}
